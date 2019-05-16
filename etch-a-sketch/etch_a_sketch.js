04ef871bc95c5d8414e4834d552471855a7805ed
Ian.Cardoso <Ian.Cardoso@targetsistemas.com.br>
2019-05-16 11:26:29 -0300

    fixed grid generator

--- etch-a-sketch/etch_a_sketch.js
+++ etch-a-sketch/etch_a_sketch.js
@@ -1,15 +1,12 @@
-let pixels = prompt('Size of the grid: ');
-
+let pixels = 100;
+//let pixels = prompt('Size of the grid: ');
 let container = document.querySelector('.container');
 
-for (i = 0; i < pixels; i++) {
-    let row = document.createElement('div');
-    row.classList.add('row');
-    container.appendChild(row);
+container.setAttribute('style', `grid-template-columns: repeat(${pixels}, 1fr);
+                                 grid-template-rows: repeat(${pixels}, 1fr);`);
 
-    for (j = 0; j < pixels; j++) {
-        let pixel = document.createElement('div');
-        pixel.classList.add('pixel');
-        row.appendChild(pixel);
-    }
+for (i = 0; i < pixels * pixels; i++) {
+    let pixel = document.createElement('div');
+    pixel.classList.add('pixel');
+    container.appendChild(pixel);
 }
